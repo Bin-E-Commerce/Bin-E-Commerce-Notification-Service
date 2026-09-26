706d9f3354e78fccf80258f85ba9671667f68ece
DaoNgocAnh25092004 <161846189+DaoNgocAnh25092004@users.noreply.github.com>
2026-09-26 14:30:53 +0700

    build(config): configure notification path aliases

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,14 +3,17 @@
 //
 
 module.exports = {
-  moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: '.',
-  testRegex: '.*\\.spec\\.ts$',
-  transform: { '^.+\\.(t|j)s$': ['ts-jest', { tsconfig: 'tsconfig.spec.json' }] },
-  moduleNameMapper: {
-    '^@common/(.*)$': '<rootDir>/../../packages/common/$1',
-  },
-  collectCoverageFrom: ['src/**/*.ts'],
-  coverageDirectory: './coverage',
-  testEnvironment: 'node',
+    moduleFileExtensions: ['js', 'json', 'ts'],
+    rootDir: '.',
+    testRegex: '.*\\.spec\\.ts$',
+    transform: {
+        '^.+\\.(t|j)s$': ['ts-jest', { tsconfig: 'tsconfig.spec.json' }],
+    },
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+        '^@common/(.*)$': '<rootDir>/../../packages/common/$1',
+    },
+    collectCoverageFrom: ['src/**/*.ts'],
+    coverageDirectory: './coverage',
+    testEnvironment: 'node',
 };
